0d41cbbda3afc6e2fc7044da809d1143ac57a508
brenomachadodomonte <brenomachadodomonte@gmail.com>
2022-05-23 09:20:23 -0300

    Adding some new order tests

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -29,19 +29,28 @@ describe('Order unit tests', () => {
 
     it('It should calculate total', () => {
 
-        const item1 = new OrderItem("1", "Item 1", 100);
-        const item2 = new OrderItem("2", "Item 2", 150);
-        const item3 = new OrderItem("3", "Item 3", 200);
+        const item1 = new OrderItem("1", "Item 1", 100, "p1", 2);
+        const item2 = new OrderItem("2", "Item 2", 150, "p2", 2);
+        const item3 = new OrderItem("3", "Item 3", 200, "p3", 5);
 
         const order = new Order("o1", "c1", [item1, item2, item3]);
         let total = order.total();
 
-        expect(total).toBe(450);
+        expect(total).toBe(1500);
 
         const order2 = new Order("o2", "c2", [item1, item2]);
         total = order2.total();
 
-        expect(total).toBe(250);
+        expect(total).toBe(500);
+
+    });
+
+    it('It should throw error when item quantity is lower than 0', () => {
+
+        expect(() => {
+            const item1 = new OrderItem("1", "Item 1", 100, "p1", 0);
+            const order = new Order("o1", "c1", [item1]);
+        }).toThrowError('Item quantity should be greater than 0');
 
     });
 
--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ const address = new Address("Rua 2", 2, "12345-678", "Teresina");
 customer.Address = address;
 customer.activate();
 
-const item1 = new OrderItem("1", "Item 1", 10);
-const item2 = new OrderItem("2", "Item 2", 15);
-const item3 = new OrderItem("3", "Item 3", 20);
+const item1 = new OrderItem("1", "Item 1", 10, "p1", 2);
+const item2 = new OrderItem("2", "Item 2", 15, "p1", 2);
+const item3 = new OrderItem("3", "Item 3", 20, "p1", 2);
 
 const order = new Order("1", "123", [item1, item2, item3]);
