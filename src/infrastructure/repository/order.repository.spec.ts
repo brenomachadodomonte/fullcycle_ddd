1846b712ce9f460aebbff1f262b5407059ca3e9f
brenomachadodomonte <brenomachadodomonte@gmail.com>
2023-02-03 15:30:58 -0300

    Fixing storage error on test

--- src/infrastructure/repository/order.repository.spec.ts
+++ src/infrastructure/repository/order.repository.spec.ts
@@ -19,7 +19,7 @@ describe('Order Repository Unit test', () => {
     beforeEach(async () => {
         sequelize = new Sequelize({
             dialect: 'sqlite',
-            storage: ':memory',
+            storage: ':memory:',
             logging: false,
             sync: { force: true }
         });
