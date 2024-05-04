723fa7a2874c9ed7b02a0b9c36e59903ef54536a
brenomachadodomonte <brenomachadodomonte@gmail.com>
2024-05-04 11:32:41 -0300

    Creating notification

---
+++ src/domain/@shared/notification/notification.ts
@@ -0,0 +1,19 @@
+export type NotificationError = {
+    message: string
+    context: string
+}
+
+export default class Notification {
+    private errors: NotificationError[] = []
+
+    addError(error: NotificationError): void {
+        this.errors.push(error)
+    }
+
+    messages(context?: string): string {
+        return this.errors
+                    .filter(error => (error.context === context || context === undefined))
+                    .map(error => `${error.context}: ${error.message}`)
+                    .join(',')
+    }
+}
